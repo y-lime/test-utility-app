f8fc40cccc21080793a8a5720218210d57235365
y-lime <y.murai.tech@gmail.com>
2023-04-02 21:04:57 +0900

    first commit

---
+++ src/main/java/com/example/testutilityapp/TestUtilityAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.testutilityapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestUtilityAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestUtilityAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testutilityapp/TestUtilityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testutilityapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestUtilityAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
