a41739f4868572523a4af9e7c0eb0885de861507
y-lime <y.murai.tech@gmail.com>
2023-04-08 16:36:47 +0900

    Logging Test

---
+++ src/main/java/com/example/testutilityapp/testutil/log/LoggingEventExtension.java
@@ -0,0 +1,85 @@
+package com.example.testutilityapp.testutil.log;
+
+import java.util.ArrayDeque;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.List;
+
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.Appender;
+
+public class LoggingEventExtension implements BeforeEachCallback, AfterEachCallback {
+
+    private static ThreadLocal<Deque<ArgumentCaptor<ILoggingEvent>>> loggingEventCaptorsHolder = new ThreadLocal<>();
+
+    private static ThreadLocal<Deque<Appender<ILoggingEvent>>> appendersHolder = new ThreadLocal<>();
+
+    // TODO ロガーごとにキューに貯める or 抽出できないか？
+
+    public static List<ILoggingEvent> getLoggingEvent() {
+        Deque<ArgumentCaptor<ILoggingEvent>> loggingEventCaptors = loggingEventCaptorsHolder.get();
+        if (loggingEventCaptors == null || loggingEventCaptors.isEmpty()) {
+            return Collections.emptyList();
+        }
+        ArgumentCaptor<ILoggingEvent> loggingEventCaptor = loggingEventCaptors.peek();
+        return Collections.unmodifiableList(loggingEventCaptor.getAllValues());
+    }
+
+    @Override
+    public void beforeEach(ExtensionContext context) throws Exception {
+        Deque<ArgumentCaptor<ILoggingEvent>> loggingEventCaptors = loggingEventCaptorsHolder.get();
+        if (loggingEventCaptors == null) {
+            loggingEventCaptors = new ArrayDeque<>();
+            loggingEventCaptorsHolder.set(loggingEventCaptors);
+        }
+        Deque<Appender<ILoggingEvent>> appenders = appendersHolder.get();
+        if (appenders == null) {
+            appenders = new ArrayDeque<>();
+            appendersHolder.set(appenders);
+        }
+
+        @SuppressWarnings("unchecked")
+        Appender<ILoggingEvent> appender = Mockito.mock(Appender.class);
+        ArgumentCaptor<ILoggingEvent> loggingEventCaptor = ArgumentCaptor.forClass(ILoggingEvent.class);
+
+        Mockito.doNothing().when(appender).doAppend(loggingEventCaptor.capture());
+
+        Logger rootLogger = (Logger) LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME);
+        rootLogger.addAppender(appender);
+
+        loggingEventCaptors.add(loggingEventCaptor);
+        appenders.add(appender);
+    }
+
+    @Override
+    public void afterEach(ExtensionContext context) throws Exception {
+        Deque<ArgumentCaptor<ILoggingEvent>> loggingEventCaptors = loggingEventCaptorsHolder.get();
+        if (loggingEventCaptors != null) {
+            loggingEventCaptors.remove();
+            if (loggingEventCaptors.isEmpty()) {
+                loggingEventCaptorsHolder.remove();
+            }
+        }
+        Deque<Appender<ILoggingEvent>> appenders = appendersHolder.get();
+        if (appenders != null) {
+            Appender<ILoggingEvent> appender = appenders.peek();
+            if (appender != null) {
+                Logger rootLogger = (Logger) LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME);
+                rootLogger.detachAppender(appender);
+            }
+            appenders.remove();
+            if (appenders.isEmpty()) {
+                appendersHolder.remove();
+            }
+        }
+    }
+
+}
---
+++ src/test/java/com/example/testutil/log/LoggingTest.java
@@ -0,0 +1,33 @@
+package com.example.testutil.log;
+
+import java.util.List;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import com.example.testutilityapp.testutil.log.LoggingEventExtension;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+
+@ExtendWith(LoggingEventExtension.class)
+class LoggingTest {
+
+    @Test
+    void logging() throws Exception {
+        org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger("slf4j");
+        logger.info("INFO: test slf4j.");
+        logger.error("ERRPR: test slf4j error.");
+
+        List<ILoggingEvent> events = LoggingEventExtension.getLoggingEvent();
+        Assertions.assertThat(events).as("count of log events").hasSize(2);
+        Assertions.assertThat(events.get(0).getLevel()).as("log level").isEqualTo(ch.qos.logback.classic.Level.INFO);
+        Assertions.assertThat(events.get(0).getLoggerName()).as("logger name").isEqualTo("slf4j");
+        Assertions.assertThat(events.get(0).getMessage()).as("log message").isEqualTo("INFO: test slf4j.");
+        Assertions.assertThat(events.get(1).getLevel()).as("log level").isEqualTo(ch.qos.logback.classic.Level.ERROR);
+        Assertions.assertThat(events.get(1).getLoggerName()).as("logger name").isEqualTo("slf4j");
+        Assertions.assertThat(events.get(1).getMessage()).as("log message").isEqualTo("ERRPR: test slf4j error.");
+
+    }
+
+}
