f418fae8a1c4d64769c177865531ff85495c699b
y-lime <y.murai.tech@gmail.com>
2023-04-16 14:53:53 +0900

    add thymeleaf app

---
+++ src/main/java/com/example/testutilityapp/controller/EchoController.java
@@ -0,0 +1,32 @@
+package com.example.testutilityapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.testutilityapp.form.EchoForm;
+
+import jakarta.validation.Valid;
+
+@Controller
+@RequestMapping("echo")
+public class EchoController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String viewInput(Model model) {
+        EchoForm form = new EchoForm();
+        model.addAttribute(form);
+        return "echo/input";
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String echo(@Valid EchoForm form, BindingResult result) {
+        if (result.hasErrors()) {
+            return "echo/input";
+        }
+        return "echo/output";
+    }
+
+}
---
+++ src/main/java/com/example/testutilityapp/form/EchoForm.java
@@ -0,0 +1,23 @@
+package com.example.testutilityapp.form;
+
+import java.io.Serializable;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+public class EchoForm implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @NotEmpty
+    @Size(max = 100)
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+}
